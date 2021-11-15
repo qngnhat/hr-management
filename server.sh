089906b5f9d51906d7e57c8343c1f775f0e60335
ngcdan <Linuss1908@gmail.com>
2021-11-15 23:54:54 +0700

    fix bugs

--- server.sh
+++ server.sh
@@ -34,8 +34,8 @@ function start() {
   then
    gradle clean
   fi
-  gradle build
-  exec "$JAVACMD" -jar ./build/libs/training-spring-0.0.1-SNAPSHOT.jar
+  gradle build &&
+  exec "$JAVACMD" -jar ./build/libs/hr-management-0.0.1-SNAPSHOT.jar 
 }
 
 function stop() {
